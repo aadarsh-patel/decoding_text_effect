6835d034a4ced8c090f6fbb1aaa0bf7f10c7dd63
aadarsh-patel <patelaadarsh0@gmail.com>
2020-03-27 01:40:57 +0530

    Added license header

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, Aadarsh Patel
+// All rights reserved.
+// This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree. 
+
 import 'package:flutter/material.dart';
 import 'package:decoding_text_effect/decoding_text_effect.dart';
 
